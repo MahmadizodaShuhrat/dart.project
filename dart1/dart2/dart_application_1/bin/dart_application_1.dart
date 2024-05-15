66dd61a7332e8d300ec46a739016f6a09f78cdbc
shuhrat <shuhratmahmadizoda1@gmail.com>
2024-05-15 19:28:07 +0500

    first

---
+++ dart1/dart2/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main() 
+{
+int time = 40;
+if(time < 20)
+{
+stdout.writeln("subh bakhhayr.");
+}
+else if(time < 15)
+{
+stdout.writeln("shom bakhayr.");
+}
+else 
+{
+stdout.writeln("ruz ba khayr");
+}
+}
---
+++ dart1/dart2/dart_application_1/bin/ex_2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main(List<int> args) {
+  int  a,sum=0;
+  int arr[a];
+  for(int i=0;i<a;i++)
+   {
+    stdin.writeln arr[i];
+   }
+  for(int i=0;i<a;i++)
+  {
+    stdout.write(sum+=arr[i]);
+  }
+}
